9573d980233865957828198a11a584f94b277559
Jake Houston <jakechouston@gmail.com>
2021-06-27 12:53:37 -0400

    updated

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,36 +3,34 @@ require 'pry'
 class ItemsController < ApplicationController
 
   def index
-    binding.pry
+    # binding.pry
     if params[:user_id] && User.find_by_id(params[:user_id])
-      binding.pry
+      # binding.pry
       user = User.find(params[:user_id])
       items = user.items
       render json: items, include: :user
-      rescue ActiveRecord::RecordNotFound => invalid
-      # binding.pry
-        render json: { errors: invalid.record.errors }, status: :not_found
-      end
     else
       items = Item.all
       render json: items, include: :user
     end
     
-  
+  rescue ActiveRecord::RecordInvalid => invalid
+    # binding.pry
+    render json: { errors: invalid.record.errors }, status: :not_found
   end
 
   def show
     item = Item.find_by_id(params[:id])
     render json: item
-  rescue ActiveRecord::RecordNotFound => invalid
-    binding.pry
+  rescue ActiveRecord::RecordInvalid => invalid
+    # binding.pry
     render json: { errors: invalid.record.errors }, status: :not_found
   end
 
   def create
     item = Item.create(item_params)
     render json: item, status: :created
-  rescue ActiveRecord::RecordNotFound => invalid
+  rescue ActiveRecord::RecordInvalid => invalid
     render json: { errors: invalid.record.errors }, status: :not_found
   end
 
